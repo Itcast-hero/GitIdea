95a5ac1df15f5ad53d8473d9af642d95afb9e86d
lichenhua <654906903@qq.com>
2020-06-16 19:14:34 +0800

    version3

--- gittest/src/cn/itcast/gitdata/GitTest.java
+++ gittest/src/cn/itcast/gitdata/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("heima ");
         System.out.println("itcast ");
+        System.out.println("china ");
     }
 
 }
