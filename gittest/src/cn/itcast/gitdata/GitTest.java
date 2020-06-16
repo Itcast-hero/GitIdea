019bf4988c400aec4c19d8a350a6fd10b5328840
lichenhua <654906903@qq.com>
2020-06-16 18:59:36 +0800

    version1

---
+++ gittest/src/cn/itcast/gitdata/GitTest.java
@@ -0,0 +1,15 @@
+package cn.itcast.gitdata;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author lichenhua
+ * @Date 2020/6/16 18:54
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("heima ");
+    }
+
+}
